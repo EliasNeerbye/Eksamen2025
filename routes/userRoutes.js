3dbe3b0bc0d39614cdb6cc04134815dac79b5d2e
Elias Neerbye <quidque@quidque.no>
2025-06-04 10:31:08 +0200

    added rate limiter

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,4 +1,18 @@
+const ratelimit = require("express-rate-limit");
+
 const router = require("express").Router();
+
+const limiter = ratelimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: {
+        success: false,
+        message: "For mange forespørsler, vennligst prøv igjen senere.",
+        data: null,
+    },
+});
+
+router.use(limiter);
 
 const createUser = require("../controllers/users/createUser");
 const getUser = require("../controllers/users/getUser");
